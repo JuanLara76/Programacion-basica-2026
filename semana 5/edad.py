bcb8b99ba8d44908a31e8aba59d1e2deeec48315
Juan Lara <juandavidlara2005@gmail.com>
2026-03-20 13:12:57 -0500

    Delete semana 5 directory

--- semana 5/edad.py
+++
@@ -1,5 +0,0 @@
-edad=int(input("Dame la edad"))
-if edad >=18:
-  print("puedes entrar a la discoteca")
-else:
-  print("vayase para su casa")
--- semana 5/nota.py
+++
@@ -1,5 +0,0 @@
-nota=float(input("pedir nota"))
-if nota >=3.0:
-  print("paso la materia")
-else:
-  print("pedistes a llorar a tu casa")
